4a29d4d7b4826eca37dcee17bbd97ff87cadd144
Marceli Janowski <marceli.janowski.ext@bbc.co.uk>
2013-01-11 16:38:27 +0000

    Add excel sheet for test

--- js/calculation.js
+++ js/calculation.js
@@ -2,9 +2,12 @@
 
 for (var i = 0; i < locations.length; i++) {
 	var distances = [];
+	var data = [];
 	for( var j = 0;j < points.length;j++) {
+		data.push(points[j]);
 		point = new Point(locations[i].latitude,locations[i].latitude);
 		distances.push(point.distance(points[j]));
 	}
+	
 	distances.sort(function (a,b) { return a.get('distance')-b.get('distance')})
 };
