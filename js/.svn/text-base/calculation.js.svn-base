54d4a8b53f29b746ac4ea0c272a959c0d35738e6
Marceli Janowski <marceli.janowski.ext@bbc.co.uk>
2013-01-14 15:23:42 +0000

    Small fixes to view

--- js/.svn/text-base/calculation.js.svn-base
+++ js/.svn/text-base/calculation.js.svn-base
@@ -8,6 +8,16 @@ for (var i = 0; i < locations.length; i++) {
 		point = new Point(locations[i].latitude,locations[i].latitude);
 		distances.push(point.distance(points[j]));
 	}
-	
-	distances.sort(function (a,b) { return a.get('distance')-b.get('distance')})
+	distances.sort(function (a,b) { return a.get('distance')-b.get('distance')});
+	var listElement = document.getElementById('pointListing');
+	var string = '';
+	for(var j = 0 ; j < 30 ; j++) {
+		string += '<tr>'+
+				   '<td>'+distances[j].get('distance')+'</td>'+
+				   '</tr>';
+
+	}
+	var header = '<tr><th>Distance from point</th></tr>';
+	string = '<table>'+header+string+'</table>';
+	listElement.innerHTML = string;
 };
--- js/calculation.js
+++ js/calculation.js
@@ -2,22 +2,23 @@
 
 for (var i = 0; i < locations.length; i++) {
 	var distances = [];
-	var data = [];
+	
 	for( var j = 0;j < points.length;j++) {
-		data.push(points[j]);
-		point = new Point(locations[i].latitude,locations[i].latitude);
-		distances.push(point.distance(points[j]));
+		point = new Point(points[j].latitude,points[j].longitude);
+		distances.push(point.distance(locations[i]));
 	}
 	distances.sort(function (a,b) { return a.get('distance')-b.get('distance')});
 	var listElement = document.getElementById('pointListing');
 	var string = '';
-	for(var j = 0 ; j < 30 ; j++) {
+	for(var j = 0 ; j < 20 ; j++) {
 		string += '<tr>'+
+				   '<td>'+distances[j].get('latitude')+'</td>'+
+				   '<td>'+distances[j].get('longitude')+'</td>'+
 				   '<td>'+distances[j].get('distance')+'</td>'+
 				   '</tr>';
 
 	}
-	var header = '<tr><th>Distance from point</th></tr>';
+	var header = '<tr><th>Langitude</th><th>Longitude</th><th>Distance (km)</th></tr>';
 	string = '<table>'+header+string+'</table>';
 	listElement.innerHTML = string;
 };
